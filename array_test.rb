c8512c23884ac45ca4999ab937cce46394caacab
Cody <codywalenciak@gmail.com>
2019-05-22 13:47:24 -0500

    Truly groundbreaking stuff. Basic array methods 🎉

---
+++ array_test.rb
@@ -0,0 +1,14 @@
+str_arr = ["line", "twine", "string", "rope", "wire"]
+numb_arr = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+puts str_arr.at(2)
+puts numb_arr.fetch(5)
+str_arr.delete("wire")
+puts str_arr.length
+puts numb_arr.join("/")
+str_arr.reverse
+numb_arr.shuffle
+numb_arr.sort
+puts.numb_arr.values_at(1,3,5).join
+puts str_arr.slice(2,4)
+numb_arr.insert(-1, 144)
